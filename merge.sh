12dc1f09ee7693861d12e0a44fbd1ad46a1c483e
Chang Luo <33987852+luochang212@users.noreply.github.com>
2024-07-06 19:17:34 +0800

    update

---
+++ merge.sh
@@ -0,0 +1,6 @@
+output='all.ipynb'
+nbmerge 1.store_embedding.ipynb \
+    2.imdb_embedding.ipynb \
+    3.sentiment_analysis_with_mlp.ipynb \
+    4.sentiment_analysis_with_transformer.ipynb \
+    > ${output}
